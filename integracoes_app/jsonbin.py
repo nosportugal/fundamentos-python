581348502e9059311055c66ad644d9d4ebfae575
Rafael Galleani <rafegal@gmail.com>
2021-06-09 12:34:46 +0100

    removido arquivos da virtualenv

--- integracoes_app/jsonbin.py
+++ integracoes_app/jsonbin.py
@@ -4,10 +4,12 @@ import os
 
 URL = "https://api.jsonbin.io/v3/b/"
 
+TOKEN = os.environ["TOKEN_JSONBIN"]
+
 
 def get_pessoa(id_pessoa):
     url_pessoa = urljoin(URL, id_pessoa)
-    headers = {"X-Master-Key": os.environ["TOKEN_JSONBIN"]}
+    headers = {"X-Master-Key": TOKEN}
     # response = requests.get(url, auth=(user, password))
     # response = requests.get(url, auth=(os.environ["USER_JSONBIN"], os.environ["PASSWORD_JSONBIN"]))
     response = requests.get(url_pessoa, headers=headers)
@@ -15,7 +17,7 @@ def get_pessoa(id_pessoa):
 
 
 def put_pessoa(id_pessoa):
-    # url = "https://api.jsonbin.io/v3/b/60bf8ff54d024768b8f34ef0"
+    # url = "https://api.jsonbin.io/v3/b/<id_pessoa>"
     url_pessoa = urljoin(URL, id_pessoa)
     response = requests.put(url_pessoa, json=pessoa)
     print(response.text)
@@ -23,21 +25,22 @@ def put_pessoa(id_pessoa):
 
 
 def delete_pessoa(id_pessoa):
-    # url = "https://api.jsonbin.io/v3/b/60bf8ff54d024768b8f34ef0"
+    # url = "https://api.jsonbin.io/v3/b/<id_pessoa>"
     url_pessoa = urljoin(URL, id_pessoa)
-    headers = {"X-Master-Key": os.environ["TOKEN_JSONBIN"]}
+    headers = {"X-Master-Key": TOKEN}
     response = requests.delete(url_pessoa, headers=headers)
     return response.status_code, response.json()
 
 
 def create_pessoa(pessoa):
-    headers = {"X-Master-Key": os.environ["TOKEN_JSONBIN"]}
+    headers = {"X-Master-Key": TOKEN}
     response = requests.post(URL, json=pessoa, headers=headers)
     return response.status_code, response.json()
 
+
 if __name__ == "__main__":
-    #pessoa = get_pessoa("60bf95ef4d024768b8f356f1")
-    #print(pessoa)
+    # pessoa = get_pessoa("<id_pessoa>")
+    # print(pessoa)
     pessoa = {"nome": "Rafael", "sobrenome": "Galleani"}
     # status, content = put_pessoa(pessoa)
     # status, content = delete_pessoa()
--- integracoes_app/telegram.py
+++ integracoes_app/telegram.py
@@ -1,14 +1,17 @@
+from urllib.parse import urljoin
 import requests
+import os
+
+TOKEN = os.environ["TOKEN_JSONBIN"]
+URL = f"https://api.telegram.org/bot{TOKEN}/"
 
 
 def send_message(text):
-    url = "https://api.telegram.org/bot1706760277:AAHEtNJqwY-gtXuUQTa2lHuL5Yl4CFzROdA/sendMessage"
-    data = {
-        "chat_id": "159062336",
-        "text": text
-    }
+    url = urljoin(URL, "sendMessage")
+    data = {"chat_id": "159062336", "text": text}
     response = requests.post(url, json=data)
     return response.status_code, response.json()
+
 
 if __name__ == "__main__":
     send_message("Olá")
